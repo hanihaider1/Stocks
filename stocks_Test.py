03ab70b668b1c44eabc6ec670fc05b5db16edb28
jindalshagun <78768060+jindalshagun@users.noreply.github.com>
2021-04-25 11:43:23 -0400

    Add files via upload

---
+++ stocks_Test.py
@@ -0,0 +1,53 @@
+import unittest
+from final_stocks import Stocks
+
+class StocksTest(unittest.TestCase):
+    
+    def test_init(self):
+        '''
+        method-- test_init
+        testing if init int takes the value correctly 
+        Parameters:
+        self -- the current object
+        return none 
+        '''
+        validation = Stocks("PYPL.csv")
+        value1,value2 = validation.name()
+        self.assertEqual(value1[0], 304.790009)
+        self.assertEqual(value1[1], 34.200001)
+        self.assertEqual(value1[2], 110.150002)
+        self.assertEqual(value1[3], 86.68)
+        
+    def test_init2(self):
+        '''
+        method-- test_init
+        testing if init int takes the value correctly 
+        Parameters:
+        self -- the current object
+        return none 
+        '''
+        validation = Stocks("PYPL.csv","SQ.csv")
+        value1,value2 = validation.stock()
+        self.assertEqual(value1[0], 304.790009)
+        self.assertEqual(value1[1], 34.200001)
+        self.assertEqual(value1[2], 110.150002)
+        self.assertEqual(value2[0], 276.570007)
+        self.assertEqual(value2[1], 8.62)
+        self.assertEqual(value2[2], 72.650002)
+        
+        
+    def test_init3(self):
+        validation = Stocks("PYPL.csv")
+        with self.assertRaises(AttributeError):
+            validation.comparing_summary()
+            
+        
+      
+        
+def main():
+    unittest.main()
+    
+    
+
+
+main()
